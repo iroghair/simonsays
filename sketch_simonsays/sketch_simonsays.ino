0d89d44c16225d2f04a191b50d768ec70219b209
Ivo Roghair <ivoroghair@gmail.com>
2022-02-28 11:59:08 +0100

    Add led control functions

--- sketch_simonsays/sketch_simonsays.ino
+++ sketch_simonsays/sketch_simonsays.ino
@@ -1,10 +1,13 @@
+void turnOn(int);
+void turnOff(int);
+void turnOffAll(int);
 
 int RLED = 9;         //    Red LED connected to digital pin 9
 int GLED = 8;         //  Green LED connected to digital pin 8
 int BLED = 7;         //   Blue LED connected to digital pin 7
 int YLED = 6;         // Yellow LED connected to digital pin 6
 int ON_DELAY  = 1000; // Time that a LED is turned on [ms]
-int OFF_DELAY = 0;    // Time that a LED is turned on [ms]
+int OFF_DELAY = 1000;    // Time that a LED is turned on [ms]
 
 void setup() {
   // put your setup code here, to run once:
@@ -13,31 +16,30 @@ void setup() {
 
 void loop() {
 
-    analogWrite(RLED,255);         
-    delay(ON_DELAY);            
-    analogWrite(RLED,0);         
+    turnOn(RLED);
+    turnOn(GLED);
+    turnOn(BLED);
+    turnOn(YLED);
+    turnOffAll();
     
-    analogWrite(GLED,255);         
-    delay(ON_DELAY);            
-    analogWrite(GLED,0);         
-    
-    analogWrite(BLED,255);         
-    delay(ON_DELAY);            
-    analogWrite(BLED,0);         
-    
-    analogWrite(YLED,255);           
-    delay(ON_DELAY);            
-    analogWrite(YLED,0);         
      
 }
 
-void turnOn(led) {
+void turnOn(int led) {
   analogWrite(led,255);
   delay(ON_DELAY);
 }
 
-void turnOff(led) {
+void turnOff(int led) {
   analogWrite(led,0);
   delay(OFF_DELAY);
 }
 
+void turnOffAll() {
+  analogWrite(RLED,0);
+  analogWrite(GLED,0);
+  analogWrite(BLED,0);
+  analogWrite(YLED,0);
+  delay(OFF_DELAY);
+
+}
